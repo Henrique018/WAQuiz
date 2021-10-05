5ad0f506093837c31e3cc83c627b2b7161433eee
henrique oliveira <henrique_trind@hotmail.com>
2021-10-04 21:18:32 -0300

    add textfield validation

--- src/components/Start/index.tsx
+++ src/components/Start/index.tsx
@@ -1,5 +1,6 @@
-import Link from 'react-router-dom';
+import * as Yup from 'yup';
 import { Typography, Button, TextField } from '@material-ui/core';
+import { useFormik } from 'formik';
 
 import QuestionsIllustration from 'components/QuestionsIustration';
 import CustomStack from 'components/CustomStack';
@@ -7,6 +8,21 @@ import CustomStack from 'components/CustomStack';
 import * as S from './styles';
 
 const Start = () => {
+  const formik = useFormik({
+    initialValues: {
+      numberOfQuestions: ''
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+    validationSchema: Yup.object({
+      numberOfQuestions: Yup.number()
+        .typeError('Please, enter numbers only')
+        .min(1)
+        .required('This is a required field')
+    })
+  });
+
   return (
     <CustomStack>
       <Typography
@@ -30,18 +46,28 @@ const Start = () => {
         How many questions do you want to answer?
       </S.CustomTypography>
 
-      <TextField
-        fullWidth
-        sx={{ my: 3 }}
-        color="secondary"
-        label="Number of questions"
-        id="numberOfQuestions"
-        inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-      />
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          sx={{ my: 3 }}
+          color="secondary"
+          label="Number of questions"
+          id="numberOfQuestions"
+          value={formik.values.numberOfQuestions}
+          onChange={formik.handleChange}
+          error={
+            formik.touched.numberOfQuestions &&
+            Boolean(formik.errors.numberOfQuestions)
+          }
+          helperText={
+            formik.touched.numberOfQuestions && formik.errors.numberOfQuestions
+          }
+        />
 
-      <Button href="/main" variant="contained" color="primary" fullWidth>
-        START
-      </Button>
+        <Button type="submit" variant="contained" color="primary" fullWidth>
+          START
+        </Button>
+      </form>
     </CustomStack>
   );
 };
